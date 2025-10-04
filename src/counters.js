22d4a924533d9337957ac8e8512d08717cf2c77e
Alex-jpeg1 <spita.alexandrum@gmail.com>
2025-10-04 17:14:37 +0300

    counters working

--- src/counters.js
+++ src/counters.js
@@ -6,17 +6,13 @@ const researchID = document.getElementById('research');
 const organismsID = document.getElementById('organisms');
 const spaceMissionsID = document.getElementById('space');
 
-async function runTimer(n, currentID)
-{
-    for(let i=0; i < n; i++)
-    {
-        const curr = document.createElement('curr');
-        curr.textContent = i;
-        currentID.appendChild(curr);
+async function runTimer(n, currentID) {
+    for (let i = 0; i < n; i++) { 
+        currentID.textContent = i;
         await new Promise(resolve => setTimeout(resolve, 1));
     }
+    currentID.textContent = n + '+';
 }
-
 runTimer(researchNumber, researchID);
 runTimer(organismsNumber, organismsID);
 runTimer(experimentsNumber, spaceMissionsID);
